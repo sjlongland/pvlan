6c636878a3e0034991dfad306c21ddf88cc50243
Stuart Longland <me@vk4msl.com>
2024-07-03 12:30:17 +1000

    secret: Pass back a salted secret object
    This keeps a binary representation of the salted secret, and provides a
    means of accessing both it and the string representation.

--- pvlan/secret.py
+++ pvlan/secret.py
@@ -11,22 +11,72 @@ import os
 import hashlib
 
 
+SALTED_SECRET_HASH_NAME = "sha256"
+SALTED_SECRET_ITERATIONS = 100
+SALTED_SECRET_SALTLEN = 32
+SALTED_SECRET_LEN = 8
+SALTED_SECRET_SEP = ":"
+SALTED_SECRET_SEPBYTES = 2
+
+
+class SaltedSecret(object):
+    """
+    A secret that has been salted.  Stores the byte representation and
+    a visual string representation of the same secret.
+    """
+
+    def __init__(
+        self,
+        hashedsecret,
+        sep=SALTED_SECRET_SEP,
+        sepbytes=SALTED_SECRET_SEPBYTES,
+    ):
+        self._hashedsecret = hashedsecret
+        self._sep = sep
+        self._sepbytes = self._sepbytes
+
+    @property
+    def sep(self):
+        """
+        Separator to use in string representation of the secret.
+        """
+        return self._sep
+
+    @property
+    def sepbytes(self):
+        """
+        Number of bytes to use between separators in the string
+        representation.
+        """
+        return self._sepbytes
+
+    def __bytes__(self):
+        return self._hashedsecret
+
+    def __repr__(self):
+        return bytes(self).hex(self.sep, self.sepbytes)
+
+
 def saltedsecret(
-    hash_name="sha256",
-    iterations=100,
-    saltlen=32,
-    length=8,
-    sep=":",
-    sepbytes=2,
+    hash_name=SALTED_SECRET_HASH_NAME,
+    iterations=SALTED_SECRET_ITERATIONS,
+    saltlen=SALTED_SECRET_SALTLEN,
+    length=SALTED_SECRET_LEN,
+    sep=SALTED_SECRET_SEP,
+    sepbytes=SALTED_SECRET_SEPBYTES,
 ):
     """
     Make a hash function that will hash the secrets passed to it.
     """
     salt = os.urandom(saltlen)
 
-    return lambda p: hashlib.pbkdf2_hmac(
-        hash_name=hash_name,
-        password=bytes(p),
-        salt=salt,
-        iterations=iterations,
-    )[:-length].hex(sep, sepbytes)
+    return lambda p: SaltedSecret(
+        hashedsecret=hashlib.pbkdf2_hmac(
+            hash_name=hash_name,
+            password=bytes(p),
+            salt=salt,
+            iterations=iterations,
+        )[:-length],
+        sep=sep,
+        sepbytes=sepbytes,
+    )
